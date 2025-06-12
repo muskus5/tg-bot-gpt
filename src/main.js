82cfc9aae908393e6f511c4077aebf00f9462547
muskus5 <oaokuznetcov@mail.ru>
2025-06-12 19:26:00 +0300

    feat(bot): add error handling for voice message processing

--- src/main.js
+++ src/main.js
@@ -5,7 +5,14 @@ import config from "config";
 const bot = new Telegraf(config.get("TELEGRAMM_TOKEN"));
 
 bot.on(message("voice"), async (ctx) => {
-  await ctx.reply(JSON.stringify(ctx.message.voice, null, 2));
+  try {
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    const userId = String(ctx.message.from.id);
+    console.log(link.href);
+    await ctx.reply(JSON.stringify(link, null, 2));
+  } catch (e) {
+    console.log(`Error while voice message`, e.message);
+  }
 });
 
 bot.command("start", async (ctx) => {
