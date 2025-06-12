1460cfecb670521a1a705d03c37b88e928f5b2db
muskus5 <oaokuznetcov@mail.ru>
2025-06-12 15:02:19 +0300

    feat: add event handlers for /start command and voice messages in bot

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import config from "config";
 
-const bot = new Telegraf("");
+const bot = new Telegraf(config.get("TELEGRAMM_TOKEN"));
+
+bot.on(message("voice"), async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message.voice, null, 2));
+});
+
+bot.command("start", async (ctx) => {
+  await ctx.reply(JSON.stringify(ctx.message, null, 2));
+});
 
 bot.launch();
 
-process.once("SIGINT", () => bot.stop('SIGINT'));
-process.once("SIGTERM", () => bot.stop('SIGTERM'));
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
