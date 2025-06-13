a28b7ef6e2d4ceaa21d19deae36b7e039899cdfa
muskus5 <oaokuznetcov@mail.ru>
2025-06-13 11:06:54 +0300

    feat: add path to current directory and file write stream

--- src/ogg.js
+++ src/ogg.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { createWriteStream } from "fs";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 class OggConverter {
   constructor() {}
@@ -7,12 +11,15 @@ class OggConverter {
 
   async create(url, filename) {
     try {
+      const oggPath = resolve(__dirname, "../voices", `${filename}.ogg`);
       const response = await axios({
         method: "get",
         url,
         responseType: "stream",
       });
-      const stream = createWriteStream();
+      const stream = createWriteStream(oggPath);
+      response.data.pipe(stream);
+      stream.on('finish', () => {})
     } catch (e) {
       console.log("Error while creating ogg", e.message);
     }
